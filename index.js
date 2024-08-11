c0981888e1e5ad6fa212cd87cce6353243ad6272
Alexsey Solyanoy <alexsey.solyanoy@gmail.com>
2024-08-11 22:11:20 +0300

    sttg

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = async (req, res) => {
       await mainBot.handleUpdate(req.body);
       res.status(200).send('OK');
     } catch (error) {
+      // Логирование ошибки и отправка ответа с кодом 500 в случае ошибки
       console.error('Error handling update:', error);
       res.status(500).send('Internal Server Error');
     }
